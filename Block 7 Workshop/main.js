18b61fb6c0e670456ce602dfb9a1491d80be6883
James <chenja@amazon.com>
2023-03-23 17:12:34 -0700

    First

---
+++ Block 7 Workshop/main.js
@@ -0,0 +1,9 @@
+// STEPS: State assignment list of requirements pseudocode, variables assigned, arithmetic operator, string data type
+//1. variable one = 10 use add
+ let one = 5 + 5;
+//2. variable two = 40 use multiply
+ let two = 5 * 8;
+//3. variable three = 39 use subtract
+ let three = 44 - 5; 
+//4. make pop up alert string You have recieved this message because you have been chosen to open an important vault. Here is the secret combination.
+alert ("You have recieved this message because you have been chosen to open an important vault. Here is the secret combination " + one + - + two + - + three + "")
